78cbb8e85334ce25828f086400566ae958e3b060
Noah Citron <noah@jeff.org>
2021-07-07 15:43:31 -0400

    add FLIRebalanceViewer deployment scripts (#64)

---
+++ deployments/constants/021_fli_viewer.ts
@@ -0,0 +1,11 @@
+export const CONTRACT_NAMES = {
+  FLI_VIEWER: "FLIRebalanceViewer",
+  ETH_FLI_VIEWER_NAME: "FLIRebalanceViewer",
+  BTC_FLI_VIEWER_NAME: "BTCFLIRebalanceViewer",
+
+  UNISWAP_V3_EXCHANGE_ADAPTER: "UniswapV3ExchangeAdapter",
+  SUSHISWAP_EXCHANGE_ADAPTER: "SushiswapExchangeAdapter",
+
+  ETH_FLI_STRATEGY_EXTENSION_NAME: "FlexibleLeverageStrategyExtension",
+  BTC_FLI_STRATEGY_EXTENSION_NAME: "BTCFlexibleLeverageStrategyExtension",
+};
---
+++ test/deploys/021_fli_viewer.spec.ts
@@ -0,0 +1,62 @@
+import "module-alias/register";
+import { deployments } from "hardhat";
+
+import {
+  addSnapshotBeforeRestoreAfterEach,
+  getAccounts,
+  getWaffleExpect,
+} from "@utils/index";
+
+import { Account } from "@utils/types";
+import { DEPENDENCY, findDependency, getContractAddress } from "@deployments/utils";
+import { CONTRACT_NAMES } from "../../deployments/constants/021_fli_viewer";
+import { FLIRebalanceViewer } from "@set/typechain/FLIRebalanceViewer";
+import { FLIRebalanceViewer__factory } from "@set/typechain/factories/FLIRebalanceViewer__factory";
+
+const expect = getWaffleExpect();
+
+const {
+  SUSHISWAP_ROUTER,
+  UNISWAP_V3_QUOTER,
+} = DEPENDENCY;
+
+describe("IndexPowah", () => {
+
+  let deployer: Account;
+  let ethFLIViewer: FLIRebalanceViewer;
+  let bctFLIViewer: FLIRebalanceViewer;
+
+  before(async () => {
+    [
+      deployer,
+    ] = await getAccounts();
+
+    await deployments.fixture();
+
+    const deployedETHFLIViewer = await getContractAddress(CONTRACT_NAMES.ETH_FLI_VIEWER_NAME);
+    ethFLIViewer = new FLIRebalanceViewer__factory(deployer.wallet).attach(deployedETHFLIViewer);
+
+    const deployedBTCFLIViewer = await getContractAddress(CONTRACT_NAMES.BTC_FLI_VIEWER_NAME);
+    bctFLIViewer = new FLIRebalanceViewer__factory(deployer.wallet).attach(deployedBTCFLIViewer);
+  });
+
+  addSnapshotBeforeRestoreAfterEach();
+
+  describe("#constructor", async () => {
+    it("should set the correct state variables fro ETH FLI", async () => {
+      expect(await ethFLIViewer.uniswapV2ExchangeName()).to.eq(CONTRACT_NAMES.SUSHISWAP_EXCHANGE_ADAPTER);
+      expect(await ethFLIViewer.uniswapV3ExchangeName()).to.eq(CONTRACT_NAMES.UNISWAP_V3_EXCHANGE_ADAPTER);
+      expect(await ethFLIViewer.uniswapV2Router()).to.eq(await findDependency(SUSHISWAP_ROUTER));
+      expect(await ethFLIViewer.uniswapV3Quoter()).to.eq(await findDependency(UNISWAP_V3_QUOTER));
+      expect(await ethFLIViewer.fliStrategyExtension()).to.eq(await findDependency(CONTRACT_NAMES.ETH_FLI_STRATEGY_EXTENSION_NAME));
+    });
+
+    it("should set the correct state variables for BTC FLI", async () => {
+      expect(await bctFLIViewer.uniswapV2ExchangeName()).to.eq(CONTRACT_NAMES.SUSHISWAP_EXCHANGE_ADAPTER);
+      expect(await bctFLIViewer.uniswapV3ExchangeName()).to.eq(CONTRACT_NAMES.UNISWAP_V3_EXCHANGE_ADAPTER);
+      expect(await bctFLIViewer.uniswapV2Router()).to.eq(await findDependency(SUSHISWAP_ROUTER));
+      expect(await bctFLIViewer.uniswapV3Quoter()).to.eq(await findDependency(UNISWAP_V3_QUOTER));
+      expect(await bctFLIViewer.fliStrategyExtension()).to.eq(await findDependency(CONTRACT_NAMES.BTC_FLI_STRATEGY_EXTENSION_NAME));
+    });
+  });
+});
